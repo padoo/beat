7ee2c44287dd74d1777280bd96960a54adc1d418
padoo <padoo@padoo.net>
2017-05-03 14:35:21 +0200

    hold button working example

--- app/main-view-model.js
+++ app/main-view-model.js
@@ -15,34 +15,56 @@ function createViewModel() {
     viewModel.message = getMessage(viewModel.counter);
     viewModel.stopped = true;
 
+    /*var holdButton = viewModel.getElementById('holdBtn');
+    holdButton.on(gestures.GestureTypes.touch, function (args) {
+        if(args.action === 'down') {
+            console.log('down');
+            httpGetArduino('red');
+        }
+
+        if (args.action === 'up') {
+            console.log('up');
+            httpGetArduino('green');
+        }
+    });*/
+
     viewModel.onTap = function () {
         this.counter--;
         this.set("message", getMessage(this.counter));
     };
 
     viewModel.onTapRed = function () {
-        http.getString('http://192.168.240.1/arduino/neo/red').then(function (response) {
-            console.log(response);
-            this.set("message", response);
-        });
+        //http.getString('http://192.168.240.1/arduino/neo/red');
+        httpGetArduino('red');
     };
 
     viewModel.onTapGreen = function () {
-        http.getString('http://192.168.240.1/arduino/neo/green');
-        this.set("message", getMessage(this.counter));
+        //http.getString('http://192.168.240.1/arduino/neo/green');
+        httpGetArduino('green');
     };
 
     viewModel.onTapBlue = function () {
-        http.getString('http://192.168.240.1/arduino/neo/blue');
-        this.set("message", getMessage(this.counter));
+        //http.getString('http://192.168.240.1/arduino/neo/blue');
+        httpGetArduino('blue');
     };
 
     viewModel.onTapGrey = function () {
-        http.getString('http://192.168.240.1/arduino/neo/reset');
-        this.set("message", getMessage(this.counter));
+        httpGetArduino('reset');
     };
 
     return viewModel;
+
+    function httpGetArduino(param) {
+        var url = 'http://192.168.240.1/arduino/neo/';
+        http.getString(url + param);
+    }
 }
 
 exports.createViewModel = createViewModel;
+
+exports.loaded = function(args){
+    console.log('hello!!');
+    var page = args.object;
+    var holdBtn = page.getViewById('holdBtn');
+    console.log(holdBtn);
+};
